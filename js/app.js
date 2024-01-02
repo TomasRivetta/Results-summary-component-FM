a372477542fade374130e9c29534254e75dfd702
TomasRivetta <totorivetta@gmail.com>
2024-01-02 20:46:15 -0300

    creando js para recorrer json

---
+++ js/app.js
@@ -0,0 +1,23 @@
+import data from "/data.json" assert {type: 'json'};
+
+const ul = document.querySelector(".container-elements-list")
+
+data.forEach(element => {
+    ul.innerHTML += `
+    <li>
+
+        <div class="container-info__secundaria-element">
+
+            <div class="container-info__secundaria-element--title">
+                <img src="${element.icon}" alt="icon ${element.category}">
+                <p>${element.category}</p>
+            </div>
+
+            <div class="container-info__secundaria-element--information">
+                <p class="stats-win">${element.score}</p><p class="stats-total">/ 100</p>
+            </div>
+        </div>
+
+    </li>
+    `
+});
