8a979443326d7b6354608e49bb617df5fb7c94fc
TomasRivetta <totorivetta@gmail.com>
2024-01-06 19:38:20 -0300

    modificando funcion para recorrer el json

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
-import data from "/data.json" assert {type: 'json'};
-
 const ul = document.querySelector(".container-elements-list")
 
-data.forEach(element => {
-    ul.innerHTML += `
-    <li>
-
-        <div class="container-info__secundaria-element" style="background:var(${element.background})">
-
-            <div class="container-info__secundaria-element--title">
-                <img src="${element.icon}" alt="icon ${element.category}">
-                <p style="color:var(${element.color});">${element.category}</p>
-            </div>
-
-            <div class="container-info__secundaria-element--information">
-                <p class="stats-win">${element.score}</p><p class="stats-total">/ 100</p>
-            </div>
-        </div>
-
-    </li>
-    `
-});
+fetch('data.json')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(element => {
+            ul.innerHTML += `
+            <li>
+        
+                <div class="container-info__secundaria-element" style="background:var(${element.background})">
+        
+                    <div class="container-info__secundaria-element--title">
+                        <img src="${element.icon}" alt="icon ${element.category}">
+                        <p style="color:var(${element.color});">${element.category}</p>
+                    </div>
+        
+                    <div class="container-info__secundaria-element--information">
+                        <p class="stats-win">${element.score}</p><p class="stats-total">/ 100</p>
+                    </div>
+                </div>
+        
+            </li>
+            `
+        })
+    })
